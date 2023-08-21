b59833782c01b938b5d2760fe6f79a843051c9b9
ANovokmet <ante.je.zakon@gmail.com>
2023-08-21 16:38:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Usage
+
+1. Clone or download this repo
+2. Navigate to repo folder
+3. `npm i --global`
+4. Run `shch` in CMD
---
+++ bin/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+require('child_process').exec('start https://youtu.be/q1-hSh10PB4?t=4');
+
+console.log(`\x1b[36m
+Mama kupila traktora, šč
+Mama kupila traktora, šč
+Mama kupila traktora
+Trajna-nina, armagedon, nona, šč
+Mama kupila traktora, šč
+Mama kupila traktora, šč
+Mama kupila traktora
+Trajna-nina, armagedon, nona
+Traktora
+
+Mama ljubila morona, šč
+Mama ljubila morona, šč
+Mama ljubila morona
+Trajna-nina, armagedon, nona
+
+A, b, c, č, ć, d, dž, đ , a, b, c, č, ć, d, dž, đ
+E, f, g, h, i, j, k, l, lj, m, n, nj
+A, b, c, č, ć, d, dž, đ, oprez, oprez
+O, p, r, s, š, t, u, v, z, ž, d, z, v, d, z
+
+Mama, mama, mama, ja se idem igrat
+Mama, idem u rat
+
+Onaj mali psihopat, r-r-rat, rat
+Mali, podli psihopat, r-r-rat, rat
+Krokodilski psihopat, r-r-rat, rat
+Mama, idem u rat
+
+Traktora
+
+Mama ljubila morona, šč
+Mama ljubila morona, šč
+Mama ljubila morona
+Trajna-nina, armagedon, nona
+
+Onaj mali psihopat
+Mali, podli psihopat, rat, rat
+Krokodilski psihopat
+Mama, idemo u rat
+Onaj mali psihopat
+Mali, podli psihopat, rat, rat
+Krokodilski psihopat
+Mama, idem u rat
+Mama, mama, mama
+Onaj mali psihopat
+Mali, podli psihopat, rat, rat
+Krokodilski psihopat
+Mama, idem u rat
+
+Šč
+\x1b[0m`);
